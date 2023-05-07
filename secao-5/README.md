b81c29917bcc4ebe2a31dd21753959b0eea34e59
conejofoz <conejofoz@gmail.com>
2023-05-06 22:48:01 -0300

    criando módulos

---
+++ secao-5/README.md
@@ -0,0 +1,2 @@
+# Secão 5 - Curso Node Hcode Treinamentos
+## Trabalhando com módulos no NodeJS
---
+++ secao-5/module01.js
@@ -0,0 +1,12 @@
+console.log('Executando module-01.js')
+
+const oculta = () => {
+    console.log('Executando função oculta')
+}
+
+module.exports.executa = () => {
+    console.log('Executando função executa')
+}
+
+module.exports.welcome = `Bem vindo módulo module-01.js`
+
