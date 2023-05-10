1ffb89cc804f65d0ceba8cff4c0488a7dd37c38a
conejofoz <conejofoz@gmail.com>
2023-05-10 20:36:51 -0300

    Usando variáveis de ambiente

--- secao-10/restapi/server.js
+++ secao-10/restapi/server.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
 /* Conexao com o bd */
-mongoose.connect("mongodb://localhost/subscribers", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (err)=>{console.error(err)})
 db.once('open', ()=>{console.log('Database Connected !!!')})
+
+/* Carregar o padrão json */
+app.use(express.json())
 
 app.listen(3000, ()=>console.log('listening on port 3000'))
