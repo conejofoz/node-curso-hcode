c32a5ef4669c42cf1cdd7f0b6cfbf1cac7c25585
conejofoz <conejofoz@gmail.com>
2023-05-10 20:31:53 -0300

    Conectando ao MongoDB

--- secao-10/restapi/server.js
+++ secao-10/restapi/server.js
@@ -2,4 +2,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+/* Conexao com o bd */
+mongoose.connect("mongodb://localhost/subscribers", { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', (err)=>{console.error(err)})
+db.once('open', ()=>{console.log('Database Connected !!!')})
+
 app.listen(3000, ()=>console.log('listening on port 3000'))
