b41a40af7ff35acabbec925545225ec772ec4a15
conejofoz <conejofoz@gmail.com>
2023-05-10 21:17:10 -0300

    Criando o model

--- secao-10/restapi/routes/subscribers.js
+++ secao-10/restapi/routes/subscribers.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {})
+router.get('/', (req, res) => {
+    res.send("rest api ok")
+})
 router.get('/:id', (req, res) => {})
 router.post('/', (req, res) => {})
 router.patch('/:id', (req, res) => {})
