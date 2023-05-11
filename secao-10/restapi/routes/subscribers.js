5abeb0e160b64c5708e45befce97f63e63ca53e8
conejofoz <conejofoz@gmail.com>
2023-05-11 00:38:08 -0300

    Atualizando e apagando

--- secao-10/restapi/routes/subscribers.js
+++ secao-10/restapi/routes/subscribers.js
@@ -7,12 +7,12 @@ router.get('/', async (req, res) => {
         const subscribers = await Subscriber.find()
         res.json(subscribers)
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', getSubscriber, (req, res) => {
+    res.json(res.subscriber)
 })
 
 router.post('/', async (req, res) => {
@@ -25,12 +25,48 @@ router.post('/', async (req, res) => {
         const newSubscriber = await subscriber.save()
         res.status(201).json(newSubscriber)
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
-   // console.log(req.body)
-   // res.status(200)
 })
-router.patch('/:id', (req, res) => {})
-router.delete('/:id', (req, res) => {})
+
+router.patch('/:id', getSubscriber, async (req, res) => {
+    if(req.body.userName != null) {
+        res.subscriber.userName = req.body.userName
+    }
+    if(req.body.userChannel != null) {
+        res.subscriber.userChannel = req.body.userChannel
+    }
+    try {
+        const updateSubscriber = await res.subscriber.save()
+        res.status(201).json(updateSubscriber)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+})
+
+router.delete('/:id', getSubscriber, async (req, res) => {
+    try {
+        await res.subscriber.deleteOne()
+        res.status(204).json({ message: 'Subscriber was removed' })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+/* Função middleware para garantir que o id existe */
+async function getSubscriber(req, res, next) {
+    let subscriber = null
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if (subscriber == null) {
+            return res.status(404).json({ message: 'Subscriber not found' })
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+
+    res.subscriber = subscriber
+    next()
+}
 
 module.exports = router
